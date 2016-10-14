48fd0166369d2cf8a59f966cd53032f2d9b06655
Irina Tisserand <dogviolet@gmail.com>
2016-10-14 14:50:44 +0200

    newChanges

--- src/js/check.js
+++ src/js/check.js
@@ -2,18 +2,15 @@ window.getMessage = function (a, b) {
 	if (typeof a === 'boolean') {
 		if (a) {
 			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-		}
-			{
+		
 		return 'Переданное GIF-изображение не анимировано';
 		}
 	}
-
-
 	if (typeof a === 'number') {
 		return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
 	}
 
-	if (Array.isArray (a)) {
+	if (Array.isArray(a)) {
 		var amountofRedPoints = 0;
 		for (var i = 0; i < a.length; i++) {
 			amountofRedPoints += a[i];
@@ -22,7 +19,7 @@ window.getMessage = function (a, b) {
 		return 'Количество красных точек во всех строчках изображения :' + amountofRedPoints;
 	}
 
-	if (Array.isArray (a) && Array.isArray (b)) {
+	if (Array.isArray(a) && Array.isArray(b)) {
 		var artifactsSquare = 0;
 		for (i < a.length; i++) {
 			artifactsSquare += a[i] * b[i];
